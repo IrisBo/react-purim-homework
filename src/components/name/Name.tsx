9d675433544407978b6366a7266947304878dda8
Iris Bolocan <nexuss13@gmail.com>
2024-03-25 16:39:54 +0200

    added name component with handle function and useState

--- src/components/name/Name.tsx
+++ src/components/name/Name.tsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import { Header} from "../header/Header";
 
-Let valueName= ''
-let myValueInput=document.querySelector(".user-name")
+import React, { useState } from "react";
 
+export function Name() {
+  // State to hold the input value
+  const [inputValue, setInputValue] = useState("");
 
-
-
-export default function Name() {
-
+  // Event handler to update the input value
+  const handleChange = (event:any) => {
+    setInputValue(event.target.value);
+  };
 
   return (
     <div>
-      
-      <p>{valueName}</p>
-
-
+      <input
+        type="text"
+        placeholder="Type your name here"
+        value={inputValue}
+        onChange={handleChange}
+      />
+      <p>{inputValue}</p>
     </div>
-  )
+  );
 }
+
+export default Name;
