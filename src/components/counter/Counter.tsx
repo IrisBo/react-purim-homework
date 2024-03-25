af61d54acfd5fb179ae3a430bbbeda5b3fedc4f5
Iris Bolocan <nexuss13@gmail.com>
2024-03-25 16:19:52 +0200

    added counter

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,7 +1,59 @@
-import React from 'react'
+// import React from 'react'
 
-export default function Counter() {
+// export default function Counter() {
+//   return (
+//     <div>Counter</div>
+//   )
+// }
+
+// import { useState } from "react";
+
+// export const Counter = () => {
+//   const [count, setCount] = useState(0);
+
+//   const handlePlus = () => {
+//     if (count < 10) {
+//       setCount(count + 1);
+//     }
+//   };
+
+//   const handleMinus = () => {
+//     if (count > 0) {
+//       setCount(count - 1);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={handlePlus}>+</button>
+//       <div className="count">{count}</div>
+//       <button onClick={handleMinus}>-</button>
+//     </div>
+//   );
+// };
+
+import { useState } from "react";
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handlePlus = () => {
+    if (count < 20) {
+      setCount(count + 1);
+    }
+  };
+
+  const handleMinus = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
-    <div>Counter</div>
-  )
-}
+    <div className="my-counter">
+      <button onClick={handlePlus}>+</button>
+      <div className="count">{count}</div>
+      <button onClick={handleMinus}>-</button>
+    </div>
+  );
+};
