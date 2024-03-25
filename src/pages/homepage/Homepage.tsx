43bd4647091bf42cf0423386d91032eae984a481
Iris Bolocan <nexuss13@gmail.com>
2024-03-25 16:22:15 +0200

    modified homepage

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Header } from '../../components/header/Header'
+import { Footer } from '../../components/footer/Footer'
+import {titleData, subTitleData} from "../../data/Homepage-header"
+import { phoneNum,currentDate, allRights } from "../../data/Homepage-footer"
+import { Layout } from '../../components/layout/Layout'
+
 
 export default function Homepage() {
   return (
-    <div>Homepage</div>
+    <div>
+      <Layout></Layout>
+
+
+ 
+      {/* <Header title={titleData} subTitle={subTitleData} />
+      <Footer contactPhone={phoneNum} currentDate={currentDate} rights={allRights}/> */}
+
+     
+    </div>
   )
 }
