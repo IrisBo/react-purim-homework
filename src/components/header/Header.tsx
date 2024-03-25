80b2994092131a50967aa9e3420ebad3d6535cbe
Iris Bolocan <nexuss13@gmail.com>
2024-03-25 16:20:12 +0200

    added header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
 
-export default function Header() {
+interface HeaderProps {
+  title: string;
+  subTitle: string;
+}
+
+export function Header(props: HeaderProps) {
+  const { title, subTitle } = props;
+
   return (
-    <div>Header</div>
-  )
+    <div>
+      <h1>{title}</h1>
+      <h3>{subTitle}</h3>
+      <input name="myInput" placeholder="type your name here" />
+      <p className="display-name"></p>
+    </div>
+  );
 }
